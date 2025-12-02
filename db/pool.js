71b47b7104f53693d257d82666ffebe095af4aa9
jerryzhiya <jerryglitter399@gmail.com>
2025-12-02 09:52:07 +0100

    Updated my connection string

--- db/pool.js
+++ db/pool.js
@@ -3,8 +3,7 @@ const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
